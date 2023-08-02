41997edc420c7fbe010091ba85b811cc812cc606
Sayak Paul <spsayakpaul@gmail.com>
2023-08-02 17:40:19 +0530

    test single

--- upscale_swin2sr/upscale_images.py
+++ upscale_swin2sr/upscale_images.py
@@ -195,6 +195,8 @@ if __name__ == "__main__":
 
     with tempfile.TemporaryDirectory() as tmpdir:
         for idx, batch in enumerate(tqdm(dataloader)):
+            if idx == 1:
+                break
             # Collate the original and edited images so that we do only a single
             # forward pass.
             images = [image for image in batch["original_image"]]
