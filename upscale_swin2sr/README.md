fd52e5ccc37a19d5935a66d2d06eb23aed6920d7
Sayak Paul <spsayakpaul@gmail.com>
2023-08-09 08:32:20 +0530

    add wds preparation script.
    Co-authored-by: Suraj Patil <surajp815@gmail.com>

--- upscale_swin2sr/README.md
+++ upscale_swin2sr/README.md
@@ -1,1 +1,4 @@
 Run `upscale_images.py` to upscale the images of [timbrooks/instructpix2pix-clip-filtered](https://huggingface.co/datasets/timbrooks/instructpix2pix-clip-filtered). The upscaler is Swin2SR from [this repository](https://github.com/mv-lab/swin2sr). `swin2sr` is entirely from the original repository. `upscale_image.py` is a modified version of `predict.py` from [here](https://github.com/mv-lab/swin2sr/blob/main/predict.py). 
+
+* `upscale_images.py` will produce a dataset (of 🤗 datasets) format locally. 
+* Then use `generate_wds_shards.py` to get the dataset converted to the `webdataset` format.
---
+++ upscale_swin2sr/generate_wds_shards.py
@@ -0,0 +1,40 @@
+import os
+
+import ray
+import webdataset as wds
+from datasets import Dataset
+
+ray.init()
+
+def main():
+    dataset_path = "/scratch/suraj/instructpix2pix-clip-filtered-upscaled"
+    wds_shards_path = "/scratch/suraj/instructpix2pix-clip-filtered-upscaled-wds"
+    # get all .arrow files in the dataset path
+    dataset_files = [os.path.join(dataset_path, f) for f in os.listdir(dataset_path) if f.endswith(".arrow")]
+
+    @ray.remote
+    def create_shard(path):
+        # get basename of the file
+        basename = os.path.basename(path)
+        # get the shard number data-00123-of-01034.arrow -> 00123
+        shard_num = basename.split("-")[1]
+        dataset = Dataset.from_file(path)
+        # create a webdataset shard
+        shard = wds.TarWriter(os.path.join(wds_shards_path, f"{shard_num}.tar"))
+        for i, example in enumerate(dataset):
+            wds_example = {
+                "__key__": str(i),
+                "original_prompt.txt": example["original_prompt"],
+                "original_image.jpg": example["original_image"].convert("RGB"),
+                "edit_prompt.txt": example["edit_prompt"],
+                "edited_prompt.txt": example["edited_prompt"],
+                "edited_image.jpg": example["edited_image"].convert("RGB"),
+            }
+            shard.write(wds_example)
+        shard.close()
+    
+    futures = [create_shard.remote(path) for path in dataset_files]
+    ray.get(futures)
+
+if __name__ == "__main__":
+    main()
